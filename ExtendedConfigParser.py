e787d36632d39a4b57c4e367dfbbd061704f93ba
fred.reichbier <fred.reichbier@486b3e87-af2d-0410-917d-c9ec71ddbe0a>
2007-09-09 15:00:16 +0000

    changed extendedconfigparser to zlib/libpng

--- ExtendedConfigParser.py
+++ ExtendedConfigParser.py
@@ -1,9 +1,30 @@
 # -*- coding: iso-8859-1 -*-
-## Extended Config Parser v1.0 ##
-## released under the terms of the GNU GPL v2 ##
-## von Friedrich Weber / Akaz, reichbier.de ##
-## besserer Configparser mit Funktionen zum Speichern und Laden ##
-## Grund von http://python.net/pipermail/python-de/2005q1/006498.html ##
+"""
+	ExtendedConfigParser, released under the zlib/libpng license
+	
+	Copyright (c) 2006-2007 Friedrich Weber
+
+	This software is provided 'as-is', without any express or implied
+	warranty. In no event will the authors be held liable for any damages
+	arising from the use of this software.
+	
+	Permission is granted to anyone to use this software for any purpose,
+	including commercial applications, and to alter it and redistribute it
+	freely, subject to the following restrictions:
+
+	    1. The origin of this software must not be misrepresented; you must not
+	    claim that you wrote the original software. If you use this software
+	    in a product, an acknowledgment in the product documentation would be
+	    appreciated but is not required.
+	
+	    2. Altered source versions must be plainly marked as such, and must not be
+	    misrepresented as being the original software.
+	
+	    3. This notice may not be removed or altered from any source
+	    distribution.
+	    
+	(gpl before)
+"""
 
 from ConfigParser import ConfigParser
 
@@ -72,10 +93,11 @@ class ExtendedConfigParser(ConfigParser):
         return res
         
 def get_and_free(filename, section, option, default=''):
-    # hole nur oben angegebenes und gib es zurück, dann
+    # hole nur oben angegebenes und gib es zurck, dann
     # gib den Parser wieder frei
     p = ExtendedConfigParser()
     p.read(filename)
     res = p.get(section, option, default)
     del p
     return res
+
